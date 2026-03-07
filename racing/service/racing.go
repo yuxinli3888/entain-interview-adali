395d58c95b2a2bf776683bba3489a6afe40a7fe0
yuxinli3888 <yuli6298@uni.sydney.edu.au>
2026-03-07 13:31:52 +1100

    add logger for service

--- racing/service/racing.go
+++ racing/service/racing.go
@@ -1,10 +1,16 @@
 package service
 
 import (
+	"errors"
+	"log"
+	"os"
+
 	"git.neds.sh/matty/entain/racing/db"
 	"git.neds.sh/matty/entain/racing/proto/racing"
 	"golang.org/x/net/context"
 )
+
+var defaultLogger = log.New(os.Stdout, "[racing-service] ", log.LstdFlags)
 
 type Racing interface {
 	// ListRaces will return a collection of races.
@@ -14,18 +20,48 @@ type Racing interface {
 // racingService implements the Racing interface.
 type racingService struct {
 	racesRepo db.RacesRepo
+	logger    *log.Logger
 }
 
 // NewRacingService instantiates and returns a new racingService.
 func NewRacingService(racesRepo db.RacesRepo) Racing {
-	return &racingService{racesRepo}
+	return NewRacingServiceWithLogger(racesRepo, defaultLogger)
+}
+
+// NewRacingServiceWithLogger instantiates a new racingService with a custom logger.
+func NewRacingServiceWithLogger(racesRepo db.RacesRepo, logger *log.Logger) Racing {
+	if logger == nil {
+		logger = defaultLogger
+	}
+
+	return &racingService{
+		racesRepo: racesRepo,
+		logger:    logger,
+	}
 }
 
 func (s *racingService) ListRaces(ctx context.Context, in *racing.ListRacesRequest) (*racing.ListRacesResponse, error) {
-	races, err := s.racesRepo.List(in.Filter)
-	if err != nil {
+	if in == nil {
+		err := errors.New("list races request is required")
+		s.logger.Printf("ListRaces failed: %v", err)
 		return nil, err
 	}
+
+	meetingIDCount := 0
+	onlyVisible := false
+	if in.Filter != nil {
+		meetingIDCount = len(in.Filter.MeetingIds)
+		onlyVisible = in.Filter.GetOnlyVisible()
+	}
+	s.logger.Printf("ListRaces called with filter: meeting_id_count=%d, only_visible=%t", meetingIDCount, onlyVisible)
+
+	races, err := s.racesRepo.List(in.Filter)
+	if err != nil {
+		s.logger.Printf("ListRaces failed while listing races: %v", err)
+		return nil, err
+	}
+
+	s.logger.Printf("ListRaces succeeded (count=%d)", len(races))
 
 	return &racing.ListRacesResponse{Races: races}, nil
 }
